40644490696ad4c40bb2e32569cdc897d89d80d0
Kavindu95 <kavindueng95@gmail.com>
2022-11-04 23:33:59 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-// import logo from './logo.svg';
-// import './App.css';
-import PartOne from './components/PartOne'
+//import logo from './logo.svg';
+import './App.css';
+import Home from './Pages/Home';
+
 
 function App() {
   return (
     <div className="App">
       <h1>This is my first react project</h1>
-      <PartOne/>
-      <PartOne/>
+      <Home/>
+      
+      
       
     </div>
   );
---
+++ src/Pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div>
+        <h1>This is website home page</h1>
+    </div>
+  )
+}
